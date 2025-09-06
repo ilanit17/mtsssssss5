d54ade3ab0a99d6394c9f8abf066a23a31a06b9b
ilanit17 <115224080+ilanit17@users.noreply.github.com>
2025-09-06 19:47:38 +0300

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FileUploadAnalyzer from './components/FileUploadAnalyzer';
+
+const App: React.FC = () => {
+    return (
+        <FileUploadAnalyzer />
+    );
+};
+
+export default App;
